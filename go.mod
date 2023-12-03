9273a2aa817519570e632f074dada3f4ea1b6334
DawenxiTech <dawenxi.tech@gmail.com>
2023-12-03 20:05:15 +0800

    optmize: add linux tray

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/dawenxi-tech/2fa
 go 1.21.4
 
 require (
+	fyne.io/systray v1.10.0
 	gioui.org v0.3.1
 	github.com/denisbrodbeck/machineid v1.0.1
 	github.com/dim13/otpauth v0.5.1
 	github.com/google/uuid v1.4.0
-	github.com/inkeliz/giosvg v0.0.0-20230915151051-c8ae55d003c6
 	github.com/makiuchi-d/gozxing v0.1.1
 	github.com/mazznoer/colorgrad v0.9.1
 	github.com/xlzd/gotp v0.1.0
@@ -18,15 +18,16 @@ require (
 	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
 	gioui.org/shader v1.0.8 // indirect
 	github.com/go-text/typesetting v0.0.0-20231113130822-cf4b5dada737 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mazznoer/csscolorparser v0.1.2 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/tevino/abool v1.2.0 // indirect
 	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
 	golang.org/x/exp/shiny v0.0.0-20231110203233-9a3e6036ecaa // indirect
 	golang.org/x/image v0.14.0 // indirect
 	golang.org/x/mobile v0.0.0-20231108233038-35478a0c49da // indirect
-	golang.org/x/net v0.0.0-20210924151903-3ad01bbaa167 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
--- tray/tray_linux.go
+++ tray/tray_linux.go
@@ -6,13 +6,24 @@ package tray
 
 import (
 	_ "embed"
+	"sync"
+	"time"
+
+	"fyne.io/systray"
+	"github.com/dawenxi-tech/2fa/storage"
+	"github.com/xlzd/gotp"
+	"golang.design/x/clipboard"
 )
 
-//go:embed 2fa-tray.ico
+//go:embed 2fa-tray-linux.png
 var iconData []byte
 
-func show_tray() {
+var once sync.Once
 
+func show_tray() {
+	once.Do(func() {
+		go systray.Run(onReady, onExit)
+	})
 }
 
 func dismiss_tray() {
@@ -28,7 +39,122 @@ func onExit() {
 }
 
 func onReady() {
+	systray.SetIcon(iconData)
+	systray.SetTitle("2FA")
 
+	var menus []*MenuItem
+
+	codes := storage.LoadCodes()
+	for _, code := range codes {
+		menu := newCodeMenu(code.Name, code.Secret.Val())
+		menu.show().listenClick()
+		menus = append(menus, menu)
+	}
+	if len(codes) > 0 {
+		systray.AddSeparator()
+	}
+	newMenuItem("Settings", EventShowSetting).show().listenClick()
+	newMenuItem("Show Window", EventShowWindow).show().listenClick()
+	newMenuItem("Quit", EventShowQuit).show().listenClick()
+
+	refreshCodes(menus)
 }
 
 func change_application_activation_policy(i int) {}
+
+const (
+	MenuItemCode = iota + 1
+	MenuItemMenu
+)
+
+type MenuItem struct {
+	title  string
+	secret string
+	event  EventType
+	typ    int
+	*systray.MenuItem
+	subMenu *systray.MenuItem
+}
+
+func (m *MenuItem) show() *MenuItem {
+	if m.MenuItem == nil {
+		switch m.typ {
+		case MenuItemCode:
+			menu := systray.AddMenuItem(m.title, "")
+			totp := gotp.NewDefaultTOTP(m.secret)
+			subMenu := menu.AddSubMenuItem(totp.Now(), "")
+			m.MenuItem = menu
+			m.subMenu = subMenu
+		case MenuItemMenu:
+			menu := systray.AddMenuItem(m.title, "")
+			m.MenuItem = menu
+		}
+	}
+
+	m.MenuItem.Show()
+	return m
+}
+
+func (m *MenuItem) listenClick() {
+	if m.MenuItem == nil {
+		return
+	}
+
+	switch m.typ {
+	case MenuItemCode:
+		m.onCodeClick()
+	case MenuItemMenu:
+		m.onMenuClick()
+	}
+}
+
+func (m *MenuItem) onCodeClick() {
+	go func() {
+		for {
+			select {
+			case <-m.MenuItem.ClickedCh:
+			case <-m.subMenu.ClickedCh:
+			}
+			clipboard.Write(clipboard.FmtText, []byte(gotp.NewDefaultTOTP(m.secret).Now()))
+		}
+	}()
+}
+
+func (m *MenuItem) onMenuClick() {
+
+	go func() {
+		for {
+			<-m.MenuItem.ClickedCh
+			Event <- m.event
+		}
+	}()
+}
+
+func newCodeMenu(title string, secret string) *MenuItem {
+	return &MenuItem{
+		title:  title,
+		secret: secret,
+		typ:    MenuItemCode,
+	}
+}
+
+func newMenuItem(title string, eventTyp EventType) *MenuItem {
+	return &MenuItem{
+		title: title,
+		typ:   MenuItemMenu,
+		event: eventTyp,
+	}
+}
+
+func refreshCodes(menus []*MenuItem) {
+	go func() {
+		time.Sleep(time.Second * (time.Duration(30 - time.Now().Second()%30)))
+		for _, m := range menus {
+			if m.subMenu == nil {
+				continue
+			}
+			code := gotp.NewDefaultTOTP(m.secret)
+			m.subMenu.SetTitle(code.Now())
+		}
+	}()
+}
